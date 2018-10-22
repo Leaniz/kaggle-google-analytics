f5ec1dca81fa342d967c72a2d66c5a43c5273641
leaniz <javier.garciadeleanizgonzalez@es.ey.com>
2018-10-22 11:45:07 +0200

    Main execution steps based on config file

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import json
+
 from src import dataset, features, models
 
-number = 1
-interim_data = dataset.make_dataset.main(number)
-processed_data = features.build_features.main(number, interim_data, save=True)
+config = json.load("./config.json")
+number = config["number"]
+
+# Calculate interim data
+if config["calculate_interim"]:
+    interim_data = dataset.make_dataset.main(number,
+                                             save=config["save_interim"])
+else:
+    interim_data = None
+
+# Calculate processed data
+if config["calculate_processed"]:
+    processed_data = features.build_features.main(number, interim_data,
+                                                  save=config["save_processed"])
+else:
+    processed_data = None
+
+# Train model
 models.train_predict.main(number, processed_data)
