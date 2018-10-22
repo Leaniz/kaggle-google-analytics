2a823228aae409b2ea29269870668e14f4082e0a
leaniz <javier.garciadeleanizgonzalez@es.ey.com>
2018-10-22 11:14:33 +0200

    Calculate average spending

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -36,7 +36,7 @@ def numeric_format(train_df, test_df, columns):
 def calc_moving_rev(row, rev_dic):
     user = row["fullVisitorId"]
     if user in rev_dic:
-        out_val = rev_dic[user]
+        out_val = float(rev_dic[user])
         rev_dic[user] += row["totals.transactionRevenue"]
         return out_val
     else:
@@ -51,6 +51,40 @@ def get_rev(row, rev_dic):
         return 0.
 
 
+def calc_avg_rev(row, rev_dic):
+    user = row["fullVisitorId"]
+    if user in rev_dic:
+        try:
+            out_val = rev_dic[user]["revenue"] / rev_dic[user]["visits"]
+        except ZeroDivisionError:
+            out_val = 0.
+        rev_dic[user]["revenue"] += row["totals.transactionRevenue"]
+        if row["totals.transactionRevenue"] > 0:
+            rev_dic[user]["visits"] += 1
+        return out_val
+    else:
+        return 0.
+
+
+def get_avg_rev(row, rev_dic):
+    user = row["fullVisitorId"]
+    if user in rev_dic:
+        try:
+            return rev_dic[user]["revenue"] / rev_dic[user]["visits"]
+        except ZeroDivisionError:
+            return 0.
+    else:
+        return 0.
+
+
+def get_relevant_visits(row, rev_dic):
+    user = row["fullVisitorId"]
+    if user in rev_dic:
+        return rev_dic[user]["visits"]
+    else:
+        return 0
+
+
 def compute_prev_revenue(train_df, test_df):
     users = set(
         train_df[train_df["totals.transactionRevenue"] > 0]["fullVisitorId"])
@@ -63,6 +97,29 @@ def compute_prev_revenue(train_df, test_df):
 
     print("Computing test set...")
     test_df["prevRevenue"] = test_df.apply(get_rev, axis=1, args=(prev_rev,))
+
+    return train_df, test_df
+
+
+def compute_avg_revenue(train_df, test_df):
+    users = set(
+        train_df[train_df["totals.transactionRevenue"] > 0]["fullVisitorId"])
+    prev_rev = {user: {"revenue": 0., "visits": 0} for user in users}
+
+    print("Computing train set...")
+    train_df.sort_values("date", inplace=True)
+    train_df["avgRevenueMoving"] = train_df.apply(calc_avg_rev, axis=1,
+                                                  args=(prev_rev,))
+    train_df["avgRevenue"] = train_df.apply(get_avg_rev, axis=1,
+                                            args=(prev_rev,))
+    train_df["relevantVisits"] = train_df.apply(get_relevant_visits, axis=1,
+                                                args=(prev_rev,))
+
+    print("Computing test set...")
+    test_df["avgRevenue"] = test_df.apply(get_avg_rev, axis=1,
+                                          args=(prev_rev,))
+    test_df["relevantVisits"] = test_df.apply(get_relevant_visits, axis=1,
+                                              args=(prev_rev,))
 
     return train_df, test_df
 
@@ -87,7 +144,8 @@ CATEGORICAL_COLS = ["channelGrouping", "device.browser",
 NUMERIC_COLS = ["totals.hits", "totals.pageviews",
                 "visitNumber", "totals.bounces",
                 "totals.newVisits", "totals.transactionRevenue",
-                "prevRevenue"]
+                # "prevRevenue"]
+                "avgRevenue", "relevantVisits"]
 
 
 def main(number=None, data=None, save=False):
@@ -104,14 +162,21 @@ def main(number=None, data=None, save=False):
     details = {}
     # Prepare processed data
     print("Preparing processed data...")
-    print("Computing prev revenue...")
+    train_df, test_df = numeric_format(train_df, test_df, NUMERIC_COLS)
+    details["Numeric formatted"] = NUMERIC_COLS
+
+    """print("Computing prev revenue...")
     t0 = time()
     train_df, test_df = compute_prev_revenue(train_df, test_df)
     print(f"{time() - t0:.2f} seconds to calculate prev revenue")
-    details["Calculated prev revenue"] = True
+    details["Calculated prev revenue"] = True"""
 
-    train_df, test_df = numeric_format(train_df, test_df, NUMERIC_COLS)
-    details["Numeric formatted"] = NUMERIC_COLS
+    print("Computing average revenue...")
+    t0 = time()
+    train_df, test_df = compute_avg_revenue(train_df, test_df)
+    print(f"{time() - t0:.2f} seconds to calculate average revenue")
+    details["Calculated final average revenue"] = True
+    details["Calculated relevant visits"] = True
 
     t0 = time()
     train_df, test_df = label_encode(train_df, test_df, CATEGORICAL_COLS)
@@ -121,8 +186,10 @@ def main(number=None, data=None, save=False):
     train_df["totals.transactionRevenue"] = np.log1p(
         train_df["totals.transactionRevenue"].values)
 
-    train_df["prevRevenue"] = np.log1p(train_df["prevRevenue"].values)
-    test_df["prevRevenue"] = np.log1p(test_df["prevRevenue"].values)
+    """train_df["prevRevenue"] = np.log1p(train_df["prevRevenue"].values)
+    test_df["prevRevenue"] = np.log1p(test_df["prevRevenue"].values)"""
+    train_df["avgRevenue"] = np.log1p(train_df["avgRevenue"].values)
+    test_df["avgRevenue"] = np.log1p(test_df["avgRevenue"].values)
 
     save_details(details, MODELS_PATH, number)
 
